994a944084bc5c2a4b2d124eaa3e6a4dc44dad39
fernieutrgv2026 <126693754+fernieutrgv2026@users.noreply.github.com>
2025-08-05 13:43:22 +0000

    commit_assig1_finaltime

--- assignments/01memorymanagement/main.rs
+++ assignments/01memorymanagement/main.rs
@@ -1,43 +1,26 @@
-fn is_even(n: i32) -> bool {
-    n % 2 == 0
+const FREEZE_POINT_FAHREN: f64 = 32.0;
+
+fn f_to_c(f: f64) -> f64 {
+    (f - FREEZE_POINT_FAHREN) * (5.0/9.0)
 }
 
-fn main() {
-    let numbers = [1,2,3,4,5,6,7,8,9,10];
-    for &num in &numbers {
-        if num % 3 == 0 && num % 5 == 0 {
-            println!("{}: FizzBuzz", num);
-        } else if num % 3 == 0 {
-            println!("{}: Fizz", num);
-        } else if num % 5 == 0 {
-            println!("{}: Buzz", num);
-        } else {
-            if is_even(num) {
-                println!("{} is even", num);
-            } else {
-                println!("{} is odd", num);
-            }
-        }
+
+fn c_to_f(c: f64) -> f64 {
+    c * (9.0/5.0) + FREEZE_POINT_FAHREN
+}
+
+fn main () {
+    let mut temp_f: f64 = 45.0;
+    let temp_c = f_to_c(temp_f);
+    println! {
+        "Temperature in celcius = {}", temp_c
+    };
+    for i in 1..=5 {
+        let curr_fahren = temp_f + i as f64;
+        let curr_celc = f_to_c(curr_fahren);
+        println! {
+            "Temperature in celcius = {}", curr_celc
+        };
     }
 
-    let mut sum = 0;
-    let mut index = 0;
-    while index < numbers.len() {
-        sum += numbers[index];
-        index += 1;
-    }
-    println!("The sum of all the numbers = {}", sum);
-
-    let mut max_value = numbers[0];
-    let mut idx = 1;
-    loop {
-        if idx >= numbers.len() {
-            break;
-        }
-        if numbers[idx] > max_value {
-            max_value = numbers[idx];
-        }
-        idx += 1;
-    }
-    println!("The largest number in the array is {}", max_value);
 }

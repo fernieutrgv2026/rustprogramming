36d6e4bdca16dc09ebb1437065760485c3d78357
fernieutrgv2026 <126693754+fernieutrgv2026@users.noreply.github.com>
2025-08-05 12:54:32 +0000

    commit_assign1

--- assignments/01memorymanagement/main.rs
+++ assignments/01memorymanagement/main.rs
@@ -1,15 +1,24 @@
+const freeze_point_fahren: f64 = 32.0;
 
-mod ownerborrow;
+fn f_to_c(f: f64) -> f64 {
+    (f - freeze_point_fahren) * (5.0/9.0)
+}
 
-fn main(){
+fn c_to_f(c: f64) -> f64 {
+    c * (9.0/5.0) + freeze_point_fahren
+}
 
-    let name = "Not provided";
-    let course = "Not Provided";
-
-    println!("Assignment: Ownership and Borrowing in Rust");
-    println!("{}",name);
-    println!("{}",course);
-
-
-
+fn main () {
+    let mut temp_f: f64 = 45.0;
+    let temp_c = f_to_c(temp_f);
+    println! {
+        "Temperature in Celcius: {:.1}", temp_c
+    };
+    for i in 1..=5 {
+        let curr_fahren = temp_f + i as f64;
+        let curr_celc = f_to_c(curr_fahren);
+        println! {
+            "Temperature in Celcius: {:.1}", curr_celc
+        };
+    }
 }
